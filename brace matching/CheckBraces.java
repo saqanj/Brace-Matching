3995d7ff397aa20e562bcf15faf6696267e9fccb
Saqlain Anjum <134897920+saqanj@users.noreply.github.com>
2023-12-29 19:27:26 -0500

    Update CheckBraces.java

--- brace matching/CheckBraces.java
+++ brace matching/CheckBraces.java
@@ -11,6 +11,9 @@ import java.nio.file.Path;
  * To execute, assuming an input file is named filename
  * and exists in the same directory as CheckBraces.class,
  * type: 'java CheckBraces filename'
+ * 
+ * @author Saqlain Anjum
+ * @version 1.0.0
  */
 public class CheckBraces {
     private static final String PUSHERS = "[({";
@@ -47,9 +50,7 @@ public class CheckBraces {
 
     /**
      * A source-code checker for missing opening and closing brackets that outputs
-     * an error message if there are any such issues during checking. Utilizes
-     * complicated boolean logic to ensure that all error possibilities are tested
-     * for.
+     * an error message if there are any such issues during checking.
      * 
      * @return The return message if use of brackets is valid within the
      *         given document.
@@ -58,10 +59,10 @@ public class CheckBraces {
         Stack<Character> symbols = new ArrayStack<>();
         String returnMessage = "";
         boolean openingError = false;
-    
+
         for (int i = 0; i < context.length(); i++) {
             char currentChar = context.charAt(i);
-    
+
             if (isPusher(currentChar)) {
                 symbols.push(currentChar);
             } else if (isClosingBracket(currentChar)) {
@@ -74,27 +75,54 @@ public class CheckBraces {
                 }
             }
         }
-    
+
         if (symbols.isEmpty() && !openingError) {
             returnMessage = ALL_MATCH;
         }
-    
+
         return returnMessage;
     }
-    
+
+    /**
+     * Utility method to check if a given character requires pushing from the stack.
+     * 
+     * @param c given character
+     * @return true if push is necessary and false otherwise.
+     */
     private boolean isPusher(char c) {
         return c == '[' || c == '(' || c == '{';
     }
-    
+
+    /**
+     * Utility method to check if a given character requires a closing bracket.
+     * 
+     * @param c given character.
+     * @return true if closing bracket is necessary and false otherwise.
+     */
     private boolean isClosingBracket(char c) {
         return c == ']' || c == ')' || c == '}';
     }
-    
+
+    /**
+     * Utility method to check if a given character matches the top bracket in the
+     * symbols Array-Stack.
+     * 
+     * @param symbols the Array-Stack.
+     * @param c       the given character.
+     * @return true if matches and false otherwise.
+     */
     private boolean matchesTopBracket(Stack<Character> symbols, char c) {
         char top = symbols.top();
         return (c == ']' && top == '[') || (c == ')' && top == '(') || (c == '}' && top == '{');
     }
-    
+
+    /**
+     * Update method for handeling any bracket errors.
+     * 
+     * @param index   given index of Array-Stack where error is occuring.
+     * @param c       given character where error is occuring.
+     * @param symbols the Array-Stack.
+     */
     private void handleBracketError(int index, char c, Stack<Character> symbols) {
         if (symbols.isEmpty()) {
             System.out.format(NO_OPENING_BRACE, index, c);
@@ -102,6 +130,5 @@ public class CheckBraces {
             System.out.format(UNEXPECTED_BRACE, index, c, symbols.top());
         }
     }
-    
 
 }
